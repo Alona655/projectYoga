582bfc850d787bb1b434eda7c14bcdbf6732dda2
Alona655 <72976145+Alona655@users.noreply.github.com>
2022-01-22 17:18:17 +0100

    Adding the first page
    This page is note done and the are the beginning of the second one.

---
+++ yoga/project/public/views/login.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="public/css/projects.css">
+    <title>INFO PAGE</title>
+</head>
+
+
+<body>
+<main>
+    <section>
+<!--            <header>-->
+<!--                <div class="container">-->
+<!--                    <div class="logo">-->
+<!--                        <p>yoga</p>-->
+<!--                    </div>-->
+<!--                    <div class="menu">-->
+<!--                        <nav>-->
+<!--                            <a class="freeTrial" href="">free trial</a>-->
+<!--                            <a class="textLogIn" href="">log in</a>-->
+<!--                            <a class="burger" href=""><img src="public/img/burger.svg"></a>-->
+<!--                        </nav>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </header>-->
+
+        <div class="content">
+            <div class="titleBlock1">
+                <h1>Programy szkoleniowe online w czasie rzeczywistym</h1>
+            </div>
+            <div class="textBlock1">
+                <p>Ulepsz swoje ciało i napełnij pozytywną energią z domowego zacisza</p>
+            </div>
+            <div class="buttonBlock1">
+                    <a class="buttonMain" href="">
+                        <p class="textButtonBlock1">WYPROBUJ ZA DARMO</p>
+                    </a>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="block2">
+            <div class="contentBlock2">
+                <div class="titleBlock2">
+                    <h2>O naszych programach szkoleniowych</h2>
+                </div>
+                <div class="textBlock2">
+                    <p>Lekcje są przeznaczone dla różnych poziomów sprawności fizycznej i są odpowiednie zarówno dla
+                        początkujących, jak i profesjonalistów. Czas trwania każdej lekcji wynosi od 15 do 60 minut.
+                        Żadne treningi nie wymagają dodatkowego sprzętu.</p>
+                </div>
+            </div>
+            <div class="imgBlock2">
+                <img src="public/img/girlOnBlock2.svg">
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="block3">
+            <div class="titleBlock3">
+                <h2>Powody, dla których warto ćwiczyć jogę</h2>
+            </div>
+            <div class="notesBlock3">
+                <div class="note1">
+                    <div class="imgNote1">
+<!--                        <img src="public/img/noteWithMoonBlock3.svg">-->
+                    </div>
+                    <div class="titleNote1">
+                        <p>Lepszy sen</p>
+                    </div>
+                    <div class="textNote1">
+                        <p>Joga zmniejsza stres, co pomaga w walce z bezsennością. Joga uczy jak odizolować umysł od
+                            codziennych problemów, dzięki temu szybciej zaśniesz i nie będziesz się budził zlękniony i
+                            zestresowany w środku nocy.</p>
+                    </div>
+                </div>
+                <div class="note2">
+                    <div class="titleNote2">
+                        <p>Nadaje elastyczność</p>
+                    </div>
+                    <div class="textNote2">
+                        <p>Praktyka jogi pobudzi do życia każdą możliwą część Twojego ciała. Zwiększa zakres ruchu i
+                            dodaje siły od stóp do głów. Wydłuża ścięgna, więzadła i mięśnie, w szczególności te
+                            głębokie, które na co dzień nie są w pełni używane. Ćwicząc regularnie zamiast napięć
+                            poczujesz przyjemne rozluźnienie.</p>
+                    </div>
+                    <div class="imgNote2">
+                        <img src="public/img/noteWithHumanBlock3.svg">
+                    </div>
+                </div>
+                <div class="note3">
+                    <div class="titleNote2">
+                        <p>Zwiększa odporność</p>
+                    </div>
+                    <div class="textNote2">
+                        <p>Сiało, które jest dotlenione i rozciągnięte dzięki swojej dobrej kondycji rzadziej
+                            (albo wcale!) zapada na choroby. Joga jest również naturalną terapią pomagającą w leczeniu
+                            reumatyzmu.</p>
+                    </div>
+                    <div class="imgNote2">
+                        <img src="public/img/noteWithHealth.svg">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="block4">
+            <div class="contentBlock4">
+                <div class="titleBlock4">
+                    <h2>O naszych programach szkoleniowych</h2>
+                </div>
+                <div class="textBlock4">
+                    <p>Lekcje są przeznaczone dla różnych poziomów sprawności fizycznej i są odpowiednie zarówno dla 
+                        początkujących, jak i profesjonalistów. Czas trwania każdej lekcji wynosi od 15 do 60 minut. 
+                        Żadne treningi nie wymagają dodatkowego sprzętu.</p>
+                </div>
+                <div class="buttonBlock4">
+                    <a class="buttonMain4" href="">
+                        <p class="textButtonBlock4">WYPROBUJ ZA DARMO</p>
+                    </a>
+                </div>
+            </div>
+            <div class="imgBlock4">
+                <img src="public/img/girlOnBlock4.svg">
+            </div>
+        </div>
+    </section>
+
+    <section>
+        <div class="block5">
+            <div class="contentBlock5">
+                <div class="titleBlock5">
+                    <h2>Wybierz swoją subscrybcję</h2>
+                </div>
+                <div class="pricesBlock5">
+                    <div class="firstPriceBlock">
+                        <div class="termFirstPrice">
+                            <p class="oneMonth"><span class="month1">1</span> miesiąc</p>
+                        </div>
+                        <div class="stick1"></div>
+                        <div class="price1Block">
+                            <p class="price1"><span class="amount1">50</span> PLN</p>
+                        </div>
+                        <div class="buttonBlock5">
+                            <a class="buttonMain5" href="">
+                                <p class="textButtonBlock5">Wybierz</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="secondPriceBlock">
+                        <div class="termSecondPrice">
+                            <p class="oneMonth2"><span class="month2">3</span> miesiące</p>
+                        </div>
+                        <div class="price2Block">
+                            <p class="price2"><span class="amount2">120</span> PLN</p>
+                        </div>
+                        <div class="buttonBlock5_2">
+                            <a class="buttonMain5_2" href="">
+                                <p class="textButtonBlock5_2">Wybierz</p>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="thirdPriceBlock">
+                        <div class="termThirdPrice">
+                            <p class="oneMonth3"><span class="month3">2</span> miesiące</p>
+                        </div>
+                        <div class="price3Block">
+                            <p class="price3"><span class="amount3">120</span> PLN</p>
+                        </div>
+                        <div class="buttonBlock5_3">
+                            <a class="buttonMain5_3" href="">
+                                <p class="textButtonBlock5_3">Wybierz</p>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <section>
+        <div class="block6">
+            <div class="contentBlock6">
+                <div class="titleBlock6">
+                    <h2>Otrzymaj miesiąc bezpłatnej usługi</h2>
+                </div>
+                <div class="emailAndButton">
+                    <div class="email">
+                        <div class="login-conteiner">
+                            <input class="login-conteiner-form" type="email" size="30" placeholder="email@email.pl">
+                        </div>
+                    </div>
+                    <div class="buttonBlock6">
+                            <a class="buttonMain6" href="">
+                                <p class="textButtonBlock6">Wybierz</p>
+                            </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</main>
+
+<footer>
+    <div class="titleFooter">
+        <p>yoga</p>
+    </div>
+    <div class="contacts">
+        <div class="profile">
+            <a>Mój profil</a>
+        </div>
+        <div class="aboutUs">
+            <a>O nas</a>
+        </div>
+        <div class="contacts_1">
+            <a>Kontakty</a>
+        </div>
+    </div>
+    <div class="media">
+        <img src="facebookBlock7.svg">
+        <img src="instagramBlock7.svg">
+        <img src="viberBlock7.svg">
+        <img src="twitterBlock7.svg">
+    </div>
+    <div class="termAndPrivacy">
+
+    </div>
+    <div class="copyrights">
+
+    </div>
+</footer>
+</body>
---
+++ yoga/project/public/views/projects.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<head>
+    <link rel="stylesheet" type="text/css" href="public/css/login.css">
+    <title>LOGIN PAGE</title>
+</head>
+
+<body>
+    <main>
+        <section>
+            <header>
+                <div class="logoLoginPage">
+                    <p>yoga</p>
+                </div>
+            </header>
+            <div class="contentLoginPage">
+                <div class="table">
+                    <div class="titleLoginPage">
+                        <p>Zaloguj się</p>
+                    </div>
+                    <div class="emailLoginPage">
+                        <div class="login-conteinerLoginPage">
+                            <input class="login-conteiner-formLoginPage" type="email" size="30" placeholder="email@email.pl">
+                        </div>
+                    </div>
+                    <div class="passwordLoginPage">
+                        <div class="password-conteinerLoginPage">
+                            <input class="password-conteiner-formLoginPage" type="password" size="30" placeholder="Password">
+                        </div>
+                    </div>
+                    <div class="buttonLoginPage">
+                        <a class="buttonMainLogin" href="">
+                            <p class="textButtonLogin">log in</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
